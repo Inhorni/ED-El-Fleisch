80bf99307811d78f1f6fbaea2f1ff22a33b42368
Nick <117300320+Inhorni@users.noreply.github.com>
2024-09-03 02:37:04 +0200

    Added sections

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,16 @@ import Hero from "./components/hero.jsx"
 import Service from "./components/service.jsx"
 import Kontakt from "./components/contact.jsx"
 import FaqSection from "./components/faq.jsx"
+import Team from "./components/team.jsx"
+import UberUns from "./components/uberuns.jsx";
 
 export default function Home() {
   return (
     <div>
          <Hero/>
          <Service/>
+         <UberUns/>
+         <Team/>
          <FaqSection/>
          <Kontakt/>
     </div>
